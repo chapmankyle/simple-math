a9db76f1c9683b6c8884f82a5f62604129084691
Kyle Chapman <kyleichapman@gmail.com>
2020-08-15 13:17:40 +0200

    Add functions to calculate min and max for three input parameters

--- include/math.hpp
+++ include/math.hpp
@@ -14,6 +14,14 @@ namespace smath {
 	}
 
 	/**
+	 * @brief Calculates the maximum between three types, comparable by operator<
+	 * @returns The larger of the three provided types.
+	 */
+	template<class T> constexpr const T& max(const T &a, const T &b, const T &c) {
+		return smath::max(a, smath::max(b, c));
+	}
+
+	/**
 	 * @brief Calculates the minimum between two types, comparable by operator<
 	 * @returns The smaller of the two provided types.
 	 */
@@ -22,9 +30,21 @@ namespace smath {
 	}
 
 	/**
+	 * @brief Calculates the minimum between three types, comparable by operator<
+	 * @returns The smaller of the three provided types.
+	 */
+	template<class T> constexpr const T& min(const T &a, const T &b, const T &c) {
+		return smath::min(a, smath::min(b, c));
+	}
+
+	/**
+	 * @brief Calculates the absolute value of the given type.
+	 *
+	 * `abs(x) = x if x >=0, or -x if x < 0`
+	 *
 	 * @returns The absolute value of the given type.
 	 */
-	template<class T> constexpr const T& abs(const T &a) {
+	template<class T> constexpr const T abs(const T &a) {
 		return a >= 0 ? a : -a;
 	}
 
