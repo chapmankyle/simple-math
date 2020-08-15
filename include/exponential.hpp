acb4e58ab4dda2369a152185abb679a5c8e72dd4
Kyle Chapman <kyleichapman@gmail.com>
2020-08-15 12:16:53 +0200

    Move sqrt method to exponential header file

---
+++ include/exponential.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#ifndef EXPONENTIAL_H
+#define EXPONENTIAL_H
+
+#include <cstdint>
+
+namespace smath {
+
+	/**
+	 * @brief Performs the fast, inverse square root of a floating-point number
+	 * with 7 digits of precision.
+	 *
+	 * Credit to Quake III: Arena for first implementation of the fast, inverse
+	 * square root.
+	 *
+	 * @returns The inverse square root of a given floating-point number, up
+	 * to 7 digits of precision.
+	 */
+	inline constexpr float inv_sqrt(const float &a) {
+		float y{ a };
+		float x{ y * 0.5f };
+
+		std::uint_fast32_t i = *(std::uint_fast32_t *) &y;
+		i = 0x5f375a86 - (i >> 1);
+		y = *(float *) &i;
+		y = y * (1.5f - (x * y * y));
+		return y;
+	}
+
+	/**
+	 * @brief Performs the fast, inverse square root of a floating-point number
+	 * with 15 digits of precision.
+	 *
+	 * Credit to https://cs.uwaterloo.ca/~m32rober/rsqrt.pdf for the extension
+	 * of the fast, inverse square root to work with higher precision.
+	 *
+	 * @returns The inverse square root of a given floating-point number, up
+	 * to 15 digits of precision.
+	 */
+	inline constexpr double inv_sqrt(const double &a) {
+		double y{ a };
+		double x{ y * 0.5 };
+
+		std::uint_fast64_t i = *(std::uint_fast64_t *) &y;
+		i = 0x5fe6eb50c7b537a9 - (i >> 1);
+		y = *(double *) &i;
+		y = y * (1.5 - (x * y * y));
+		return y;
+	}
+
+	/**
+	 * @brief Performs the square root of a floating-point number, using the
+	 * fast, inverse square root method.
+	 *
+	 * @returns The square root of the provided floating-point number, up to
+	 * 7 digits of precision.
+	 */
+	inline constexpr float sqrt(const float &a) {
+		return 1.0f / inv_sqrt(a);
+	}
+
+	/**
+	 * @brief Performs the square root of a floating-point number, using the
+	 * fast, inverse square root method.
+	 *
+	 * @returns The square root of the provided floating-point number, up to
+	 * 15 digits of precision.
+	 */
+	inline constexpr double sqrt(const double &a) {
+		return 1.0 / inv_sqrt(a);
+	}
+
+} // namespace smath
+
+#endif
--- include/math.hpp
+++ include/math.hpp
@@ -1,14 +1,9 @@
 #pragma once
-#ifndef INCLUDE_MATH_H
 
-#include <cstdint>
+#ifndef MATH_H
+#define MATH_H
 
 namespace smath {
-
-	// math constants
-
-	inline constexpr double e{ 2.7182818284590452354 };
-	inline constexpr double pi{ 3.1415926535897932385 };
 
 	/**
 	 * @brief Calculates the maximum between two types, comparable by operator<
@@ -29,32 +24,10 @@ namespace smath {
 	/**
 	 * @returns The absolute value of the given type.
 	 */
-	template<class T> constexpr const T& abs(const T& a) {
+	template<class T> constexpr const T& abs(const T &a) {
 		return a >= 0 ? a : -a;
-	}
-
-	/**
-	 * @brief Performs the square root of a floating-point number, using the
-	 * fast, inverse square root method.
-	 *
-	 * Credit to https://cs.uwaterloo.ca/~m32rober/rsqrt.pdf
-	 *
-	 * @returns The square root of the provided floating-point number.
-	 */
-	inline constexpr double sqrt(const double &a) {
-		constexpr double threehalfs{ 1.5 };
-
-		double y{ a };
-		double x{ y * 0.5 };
-
-		std::uint_fast64_t i = *(std::uint_fast64_t *) &y;
-		i = 0x5fe6eb50c7b537a9 - (i >> 1);
-		y = *(double *) &i;
-		y = y * (threehalfs - (x * y * y));
-		y = y * (threehalfs - (x * y * y));
-		return 1.0 / y;
 	}
 
 } // namespace smath
 
-#endif // INCLUDE_MATH_H
+#endif // MATH_H
