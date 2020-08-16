07e4e5c8cedfd7f5fa88fb7e1abea50bdd19c793
Kyle Chapman <kyleichapman@gmail.com>
2020-08-16 22:39:17 +0200

    Add qualifier to define a vector

---
+++ include/detail/qualifier.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef DETAIL_QUALIFIER_H
+#define DETAIL_QUALIFIER_H
+
+#include <cassert>
+
+namespace smath {
+
+	// ---------------
+	// --- vectors ---
+	// ---------------
+	// Supports:
+	// - 2 components (x, y)
+	// - 3 components (x, y, z)
+	// - 4 components (x, y, z, w)
+
+	/**
+	 * General vector of arbitrary length and type.
+	 * @tparam L The length of the vector, in range [2, 4]
+	 * @tparam T The type of data to store in the vector (float, double or integer)
+	 */
+	template<int L, class T> struct vec;
+
+} // namespace smath
+
+#endif
