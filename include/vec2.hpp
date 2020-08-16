5d9acdb63e7ad43327b459d3bb0fd6c027bf502e
Kyle Chapman <kyleichapman@gmail.com>
2020-08-16 22:42:21 +0200

    Add file to include all vec2 aliases

---
+++ include/vec2.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef VEC2_H
+#define VEC2_H
+
+#include "alias/vec2_bool.hpp"
+#include "alias/vec2_double.hpp"
+#include "alias/vec2_float.hpp"
+#include "alias/vec2_integer.hpp"
+
+#endif
