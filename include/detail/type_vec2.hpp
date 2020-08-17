317ed1045f71d55cdca09a8dfe3e879965013f06
Kyle Chapman <kyleichapman@gmail.com>
2020-08-17 17:02:33 +0200

    Move implementation of functions into separate file

--- include/detail/type_vec2.hpp
+++ include/detail/type_vec2.hpp
@@ -15,86 +15,6 @@ namespace smath {
 		T x;
 		T y;
 
-		// -- Constructors --
-
-		/**
-		 * Default constructor for a 2-component vector.
-		 */
-		constexpr vec()
-			: x{}, y{}
-		{}
-
-		/**
-		 * Constructor to initialize a vector to a single scalar.
-		 * @tparam T The type of the vector.
-		 * @param scalar The scalar value to initialize the vector to.
-		 */
-		constexpr vec(T scalar)
-			: x(scalar), y(scalar)
-		{}
-
-		/**
-		 * Constructor to initialize each component in the vector.
-		 * @tparam T The type of the vector.
-		 * @param _x The x component of the vector.
-		 * @param _y The y component of the vector.
-		 */
-		constexpr vec(T _x, T _y)
-			: x(_x), y(_y)
-		{}
-
-		/**
-		 * Constructor to initialize each component in the vector using
-		 * different types
-		 */
-		template<class A, class B>
-		constexpr vec(A _x, B _y)
-			: x(static_cast<T>(_x))
-			, y(static_cast<T>(_y))
-		{}
-
-		/**
-		 * Constructor to initialize a vector to another vector.
-		 * @param v The vector initialize to.
-		 */
-		constexpr vec(const vec<2, T> &v)
-			: x(v.x), y(v.y)
-		{}
-
-		/**
-		 * Constructor to initialize a vector to a vector from another type.
-		 * @param v The vector of a different type.
-		 */
-		template<class A>
-		constexpr vec(const vec<2, A> &v)
-			: x(static_cast<T>(v.x))
-			, y(static_cast<T>(v.y))
-		{}
-
-		// -- Element Accesses --
-
-		constexpr T& operator[](int i) {
-			assert(i >= 0 && i < this->length());
-			switch(i) {
-				default:
-				case 0:
-					return x;
-				case 1:
-					return y;
-			}
-		}
-
-		constexpr const T& operator[](int i) const {
-			assert(i >= 0 && i < this->length());
-			switch(i) {
-				default:
-				case 0:
-					return x;
-				case 1:
-					return y;
-			}
-		}
-
 		/**
 		 * @returns The number of components that the vector contains.
 		 */
@@ -102,81 +22,165 @@ namespace smath {
 			return 2;
 		}
 
+		// -- Constructors --
+
+		/**
+		 * Default constructor for a 2-component vector.
+		 */
+		constexpr vec();
+
+		/**
+		 * Constructor to initialize a vector to a single scalar.
+		 * @tparam T The type of the vector.
+		 * @param scalar The scalar value to initialize the vector to.
+		 */
+		constexpr vec(T scalar);
+
+		/**
+		 * Constructor to initialize each component in the vector.
+		 * @tparam T The type of the vector.
+		 * @param _x The x component of the vector.
+		 * @param _y The y component of the vector.
+		 */
+		constexpr vec(T _x, T _y);
+
+		/**
+		 * Constructor to initialize each component in the vector using
+		 * different parameter types.
+		 * @tparam A Some data type that is not the same base data type.
+		 * @tparam B Some data type that is not the same base data type.
+		 * @param _x The x component of the vector.
+		 * @param _y The y component of the vector.
+		 */
+		template<class A, class B>
+		constexpr vec(A _x, B _y);
+
+		/**
+		 * Constructor to initialize a vector to another vector.
+		 * @param v The vector initialize to.
+		 */
+		constexpr vec(const vec<2, T> &v);
+
+		/**
+		 * Constructor to initialize a vector to a vector from another type.
+		 * @tparam A Some data type that is not the same as the base data type.
+		 * @param v The vector of a different type.
+		 */
+		template<class A>
+		constexpr vec(const vec<2, A> &v);
+
+		// -- Element accesses --
+
+		constexpr T& operator[](int i);
+		constexpr const T& operator[](int i) const;
+
 		// -- Unary arithmetic operators --
 
-		constexpr vec<2, T> operator+(const vec<2, T> &v) {
-			return v;
-		}
-
-		constexpr vec<2, T> operator-(const vec<2, T> &v) {
-			return vec<2, T>(-v.x, -v.y);
-		}
+		template<class A>
+		constexpr vec<2, T>& operator=(const vec<2, A> &v);
 
 		template<class A>
-		constexpr vec<2, T>& operator=(const vec<2, A> &v) {
-			this->x = static_cast<T>(v.x);
-			this->y = static_cast<T>(v.y);
-			return *this;
-		}
+		constexpr vec<2, T>& operator+=(A scalar);
+		template<class A>
+		constexpr vec<2, T>& operator+=(const vec<2, A> &v);
 
 		template<class A>
-		constexpr vec<2, T>& operator+=(A scalar) {
-			this->x += static_cast<T>(scalar);
-			this->y += static_cast<T>(scalar);
-			return *this;
-		}
+		constexpr vec<2, T>& operator-=(A scalar);
+		template<class A>
+		constexpr vec<2, T>& operator-=(const vec<2, A> &v);
 
 		template<class A>
-		constexpr vec<2, T>& operator+=(const vec<2, A> &v) {
-			this->x += static_cast<T>(v.x);
-			this->y += static_cast<T>(v.y);
-			return *this;
-		}
+		constexpr vec<2, T>& operator*=(A scalar);
+		template<class A>
+		constexpr vec<2, T>& operator*=(const vec<2, A> &v);
 
 		template<class A>
-		constexpr vec<2, T>& operator-=(A scalar) {
-			this->x -= static_cast<T>(scalar);
-			this->y -= static_cast<T>(scalar);
-			return *this;
-		}
+		constexpr vec<2, T>& operator/=(A scalar);
+		template<class A>
+		constexpr vec<2, T>& operator/=(const vec<2, A> &v);
+
+		// -- Unary bit operators --
 
 		template<class A>
-		constexpr vec<2, T>& operator-=(const vec<2, A> &v) {
-			this->x -= static_cast<T>(v.x);
-			this->y -= static_cast<T>(v.y);
-			return *this;
-		}
+		constexpr vec<2, T>& operator%=(A scalar);
+		template<class A>
+		constexpr vec<2, T>& operator%=(const vec<2, A> &v);
 
 		template<class A>
-		constexpr vec<2, T>& operator*=(A scalar) {
-			this->x *= static_cast<T>(scalar);
-			this->y *= static_cast<T>(scalar);
-			return *this;
-		}
+		constexpr vec<2, T>& operator&=(A scalar);
+		template<class A>
+		constexpr vec<2, T>& operator&=(const vec<2, A> &v);
 
 		template<class A>
-		constexpr vec<2, T>& operator*=(const vec<2, A> &v) {
-			this->x *= static_cast<T>(v.x);
-			this->y *= static_cast<T>(v.y);
-			return *this;
-		}
+		constexpr vec<2, T>& operator|=(A scalar);
+		template<class A>
+		constexpr vec<2, T>& operator|=(const vec<2, A> &v);
 
 		template<class A>
-		constexpr vec<2, T>& operator/=(A scalar) {
-			this->x /= static_cast<T>(scalar);
-			this->y /= static_cast<T>(scalar);
-			return *this;
-		}
+		constexpr vec<2, T>& operator^=(A scalar);
+		template<class A>
+		constexpr vec<2, T>& operator^=(const vec<2, A> &v);
 
 		template<class A>
-		constexpr vec<2, T>& operator/=(const vec<2, A> &v) {
-			this->x /= static_cast<T>(v.x);
-			this->y /= static_cast<T>(v.y);
-			return *this;
-		}
+		constexpr vec<2, T>& operator<<=(A scalar);
+		template<class A>
+		constexpr vec<2, T>& operator<<=(const vec<2, A> &v);
+
+		template<class A>
+		constexpr vec<2, T>& operator>>=(A scalar);
+		template<class A>
+		constexpr vec<2, T>& operator>>=(const vec<2, A> &v);
+
+		// -- Increment and decrement operators --
+
+		constexpr vec<2, T>& operator++();
+		constexpr vec<2, T> operator++(int);
+
+		constexpr vec<2, T>& operator--();
+		constexpr vec<2, T> operator--(int);
 
 	};
 
+	// -- Unary arithmetic operators --
+
+	template<class T>
+	constexpr vec<2, T> operator+(const vec<2, T> &v);
+
+	template<class T>
+	constexpr vec<2, T> operator-(const vec<2, T> &v);
+
+	// -- Binary arithmetic operators --
+
+	template<class T>
+	constexpr vec<2, T> operator+(const vec<2, T> &v, T scalar);
+	template<class T>
+	constexpr vec<2, T> operator+(T scalar, const vec<2, T> &v);
+	template<class T>
+	constexpr vec<2, T> operator+(const vec<2, T> &v1, const vec<2, T> &v2);
+
+	template<class T>
+	constexpr vec<2, T> operator-(const vec<2, T> &v, T scalar);
+	template<class T>
+	constexpr vec<2, T> operator-(T scalar, const vec<2, T> &v);
+	template<class T>
+	constexpr vec<2, T> operator-(const vec<2, T> &v1, const vec<2, T> &v2);
+
+	template<class T>
+	constexpr vec<2, T> operator*(const vec<2, T> &v, T scalar);
+	template<class T>
+	constexpr vec<2, T> operator*(T scalar, const vec<2, T> &v);
+	template<class T>
+	constexpr vec<2, T> operator*(const vec<2, T> &v1, const vec<2, T> &v2);
+
+	template<class T>
+	constexpr vec<2, T> operator/(const vec<2, T> &v, T scalar);
+	template<class T>
+	constexpr vec<2, T> operator/(T scalar, const vec<2, T> &v);
+	template<class T>
+	constexpr vec<2, T> operator/(const vec<2, T> &v1, const vec<2, T> &v2);
+
 } // namespace smath
+
+#include "type_vec2.inl"
 
 #endif
