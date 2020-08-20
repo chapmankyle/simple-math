a6a030166d817a9251e7e728c9ffadad64b70a06
Kyle Chapman <kyleichapman@gmail.com>
2020-08-20 14:29:28 +0200

    Add vector1 aliases

---
+++ include/alias/vec1_float.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef ALIAS_VEC1_FLOAT_H
+#define ALIAS_VEC1_FLOAT_H
+
+#include "../types/type_vec1.hpp"
+
+namespace smath {
+
+	// Single-precision floating-point vector with 1 component
+	using vec1 = vec<1, float>;
+
+} // namespace smath
+
+#endif
---
+++ include/alias/vec1_integer.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef ALIAS_VEC1_INTEGER_H
+#define ALIAS_VEC1_INTEGER_H
+
+#include "../types/type_vec1.hpp"
+
+namespace smath {
+
+	// Integer vector with 1 component
+	using vec1i = vec<1, int>;
+
+} // namespace smath
+
+#endif
