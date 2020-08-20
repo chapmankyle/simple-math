0501ddf6be63d3feb98c9fd34a330c904df4b4c4
Kyle Chapman <20703236@sun.ac.za>
2020-08-20 12:38:52 +0200

    Add all includes into vec3.hpp

---
+++ include/vec3.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef VEC3_H
+#define VEC3_H
+
+#include "alias/vec3_bool.hpp"
+#include "alias/vec3_double.hpp"
+#include "alias/vec3_float.hpp"
+#include "alias/vec3_integer.hpp"
+
+#endif
