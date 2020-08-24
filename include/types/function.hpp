861ab93631794f0d21a97528f7089a6885d14b33
Kyle Chapman <kyleichapman@gmail.com>
2020-08-24 22:56:59 +0200

    Add methods to apply a function to a vector

---
+++ include/types/function.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include "qualifier.hpp"
+
+namespace smath {
+
+	namespace function {
+
+		/**
+		 * @brief Template class to execute a function with a single (one) parameter.
+		 * @tparam L The number of components of the vector.
+		 * @tparam T The type of the vector (bool, int, double, float)
+		 */
+		template<template<length_t L, class T> class vec, length_t L, class A, class T>
+		struct one{};
+
+		template<template<length_t L, class T> class vec, class A, class T>
+		struct one<vec, 1, A, T> {
+			SMATH_CONSTEXPR static vec<1, A> apply(A (*func) (T x), const vec<1, T> &v) {
+				return vec<1, A>(func(v.x));
+			}
+		};
+
+		template<template<length_t L, class T> class vec, class A, class T>
+		struct one<vec, 2, A, T> {
+			SMATH_CONSTEXPR static vec<2, A> apply(A (*func) (T x), const vec<2, T> &v) {
+				return vec<2, A>(func(v.x), func(v.y));
+			}
+		};
+
+		template<template<length_t L, class T> class vec, class A, class T>
+		struct one<vec, 3, A, T> {
+			SMATH_CONSTEXPR static vec<3, A> apply(A (*func) (T x), const vec<3, T> &v) {
+				return vec<3, A>(func(v.x), func(v.y), func(v.z));
+			}
+		};
+
+		/**
+		 * @brief Template class to execute a function with two parameters.
+		 * @tparam L The number of components of the vector.
+		 * @tparam T The type of the vector (bool, int, double, float)
+		 */
+		template<template<length_t L, class T> class vec, length_t L, class T>
+		struct two{};
+
+		template<template<length_t L, class T> class vec, class T>
+		struct two<vec, 1, T> {
+			SMATH_CONSTEXPR static vec<1, T> apply(T (*func) (T x, T y), const vec<1, T> &a, const vec<1, T> &b) {
+				return vec<1, T>(func(a.x, b.x));
+			}
+		};
+
+		template<template<length_t L, class T> class vec, class T>
+		struct two<vec, 2, T> {
+			SMATH_CONSTEXPR static vec<2, T> apply(T (*func) (T x, T y), const vec<2, T> &a, const vec<2, T> &b) {
+				return vec<2, T>(func(a.x, b.x), func(a.y, b.y));
+			}
+		};
+
+		template<template<length_t L, class T> class vec, class T>
+		struct two<vec, 3, T> {
+			SMATH_CONSTEXPR static vec<3, T> apply(T (*func) (T x, T y), const vec<3, T> &a, const vec<3, T> &b) {
+				return vec<3, T>(func(a.x, b.x), func(a.y, b.y), func(a.z, b.z));
+			}
+		};
+
+	} // namespace function
+
+} // namespace smath
+
+#endif
