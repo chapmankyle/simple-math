dce27f6c10650fdad773f4e4813bcecc1349eac9
Kyle Chapman <kyleichapman@gmail.com>
2020-08-24 17:25:48 +0200

    Add platform identification and setup macros

---
+++ include/setup/setup.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef SETUP_H
+#define SETUP_H
+
+#include "platform.hpp"
+
+//  __      __           _
+//  \ \    / /          (_)
+//   \ \  / /__ _ __ ___ _  ___  _ __
+//    \ \/ / _ \ '__/ __| |/ _ \| '_ \
+//     \  /  __/ |  \__ \ | (_) | | | |
+//      \/ \___|_|  |___/_|\___/|_| |_|
+//
+
+#define SMATH_VERSION_MAJOR 0
+#define SMATH_VERSION_MINOR 1
+#define SMATH_VERSION_PATCH 2
+#define SMATH_VERSION_MESSAGE "smath v0.1.2"
+
+#define SMATH_DISABLE 0
+#define SMATH_ENABLE  1
+
+#endif // SETUP_H
