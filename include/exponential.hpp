1004dc8606d3236c2926d2448de4037c30115549
Kyle Chapman <kyleichapman@gmail.com>
2020-08-24 22:57:30 +0200

    Modify sqrt method to allow vector as parameter

--- include/exponential.hpp
+++ include/exponential.hpp
@@ -3,10 +3,17 @@
 #ifndef EXPONENTIAL_H
 #define EXPONENTIAL_H
 
+#include <cmath>
 #include <cstdint>
 
-#include "constants.hpp"
+#include "setup/setup.hpp"
+#include "types/function.hpp"
+
 #include "template_types.hpp"
+
+#include "types/type_vec1.hpp"
+#include "types/type_vec2.hpp"
+#include "types/type_vec3.hpp"
 
 namespace smath {
 
@@ -20,13 +27,14 @@ namespace smath {
 	 * @returns The inverse square root of a given floating-point number, up
 	 * to 7 digits of precision.
 	 */
-	inline constexpr float inv_sqrt(const float &a) {
+	SMATH_CONSTEXPR float inv_sqrt(const float &a) {
 		float y{ a };
 		float x{ y * 0.5f };
 
 		std::uint_fast32_t i = *(std::uint_fast32_t *) &y;
 		i = 0x5f375a86 - (i >> 1);
 		y = *(float *) &i;
+		y = y * (1.5f - (x * y * y));
 		y = y * (1.5f - (x * y * y));
 		return y;
 	}
@@ -41,7 +49,7 @@ namespace smath {
 	 * @returns The inverse square root of a given floating-point number, up
 	 * to 15 digits of precision.
 	 */
-	inline constexpr double inv_sqrt(const double &a) {
+	SMATH_CONSTEXPR double inv_sqrt(const double &a) {
 		double y{ a };
 		double x{ y * 0.5 };
 
@@ -49,21 +57,31 @@ namespace smath {
 		i = 0x5fe6eb50c7b537a9 - (i >> 1);
 		y = *(double *) &i;
 		y = y * (1.5 - (x * y * y));
+		y = y * (1.5 - (x * y * y));
 		return y;
 	}
 
 	/**
-	 * @brief Performs the square root of a floating-point number, using the
-	 * fast, inverse square root method.
+	 * @brief Performs the square root of a floating-point number.
 	 * @returns The square root of the floating-point input number.
 	 */
 	template<class T>
-	constexpr const T sqrt(const T &a) {
-		static_assert(smath::is_floating_type<T>::value, "'sqrt' only accepts floating-point inputs");
-		if (a == static_cast<T>(2.0)) {
-			return static_cast<T>(smath::SQRT2);
-		}
-		return static_cast<T>(1.0) / inv_sqrt(a);
+	SMATH_CONSTEXPR const T sqrt(const T &a) {
+		return ::std::sqrt(a);
+	}
+
+	/**
+	 * @brief Performs the square root on all of the components of a vector.
+	 * @tparam L The number of components in the vector (1 <= L <= 4)
+	 * @tparam T The type of the vector (bool, int, float, double)
+	 * @param v The vector to apply the function to.
+	 * @returns A vector containing the square root of all the components.
+	 */
+	template<length_t L, class T>
+	SMATH_CONSTEXPR vec<L, T> sqrt(const vec<L, T> &v) {
+		SMATH_STATIC_ASSERT(smath::is_integer_type<T>::value || smath::is_floating_type<T>::value, "'sqrt' only accepts an integer or floating-point vector");
+		SMATH_STATIC_ASSERT(L > 0 && L < 4, "'sqrt' currently only works on vectors with 1 to 3 components");
+		return function::one<vec, L, T, T>::apply(::std::sqrt, v);
 	}
 
 } // namespace smath
