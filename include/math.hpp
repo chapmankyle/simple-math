8ddc365495bd42a82b76817f7e9f46b621f13445
Kyle Chapman <kyleichapman@gmail.com>
2020-08-27 11:04:39 +0200

    Overload exisiting functions to accept vectors as parameters

--- include/math.hpp
+++ include/math.hpp
@@ -3,7 +3,11 @@
 #ifndef MATH_H
 #define MATH_H
 
-#include "setup/setup.hpp"
+#include <cmath>
+
+#include "detail/setup.hpp"
+#include "detail/function.hpp"
+
 #include "template_types.hpp"
 
 namespace smath {
@@ -46,10 +50,7 @@ namespace smath {
 
 	/**
 	 * @brief Calculates the absolute value of the given type.
-	 *
-	 * `abs(x) = x if x >=0, or -x if x < 0`
-	 *
-	 * @returns The absolute value of the given type.
+	 * @returns `a` if `a` >= 0 else `-a`.
 	 */
 	template<class T>
 	SMATH_CONSTEXPR const T abs(const T &a) {
@@ -58,64 +59,120 @@ namespace smath {
 	}
 
 	/**
+	 * @brief Converts each component of a vector into the absolute value of that component.
+	 * @tparam L The number of components in the vector in range [1, 4]
+	 * @tparam T The type of the vector (int, float, double)
+	 * @returns A vector of length `L` and type `T` with each component as a positive number.
+	 */
+	template<length_t L, class T>
+	SMATH_CONSTEXPR vec<L, T> abs(const vec<L, T> &v) {
+		SMATH_STATIC_ASSERT(smath::is_integer_type<T>::value || smath::is_floating_type<T>::value, "'abs' only accepts integer or floating-point inputs");
+		SMATH_STATIC_ASSERT(smath::is_valid_vector(L), "'abs' only works on vectors with 1 to 4 components");
+		return function::one<vec, L, T, T>::apply(::std::abs, v);
+	}
+
+	/**
 	 * @brief Rounds a number to the nearest integer.
-	 * @returns An integer that is closest to the given input number.
+	 * @tparam T The type of number (int, float, double)
+	 * @returns A number that is closest to the given input number.
 	 */
 	template<class T>
-	SMATH_CONSTEXPR int round(const T &a) {
+	SMATH_CONSTEXPR T round(const T &a) {
 		SMATH_STATIC_ASSERT(smath::is_integer_type<T>::value || smath::is_floating_type<T>::value, "'round' only accepts integer or floating-point inputs");
-		return static_cast<int>(a + 0.5);
+		return static_cast<T>(static_cast<int>(a + 0.5));
+	}
+
+	/**
+	 * @brief Rounds each component of a vector to the nearest whole number.
+	 * @tparam L The number of components in the vector in range [1, 4]
+	 * @tparam T The type of the vector (int, float, double)
+	 * @returns A vector of length `L` and type `T` with each component rounded
+	 * to the nearest whole number.
+	 */
+	template<length_t L, class T>
+	SMATH_CONSTEXPR vec<L, T> round(const vec<L, T> &v) {
+		SMATH_STATIC_ASSERT(smath::is_integer_type<T>::value || smath::is_floating_type<T>::value, "'round' only accepts integer or floating-point inputs");
+		SMATH_STATIC_ASSERT(smath::is_valid_vector(L), "'round' only works on vectors with 1 to 4 components");
+		return function::one<vec, L, T, T>::apply(::std::round, v);
 	}
 
 	/**
 	 * @brief Rounds a number to the nearest multiple of the `nearest` input parameter.
+	 * @tparam T The type of number to round.
 	 * @param a The number to round.
 	 * @param nearest The number to round up or down to.
 	 * @returns The input rounded to the nearest multiple of the input parameter.
 	 */
 	template<class T>
-	SMATH_CONSTEXPR int round_nearest(const T &a, const int &nearest) {
+	SMATH_CONSTEXPR T round_nearest(const T &a, const int &nearest) {
 		SMATH_STATIC_ASSERT(smath::is_integer_type<T>::value || smath::is_floating_type<T>::value, "'round_nearest' only accept integers or floating-point inputs");
-		return static_cast<int>(smath::round(static_cast<double>(a) / nearest) * nearest);
+		return static_cast<T>(static_cast<int>(smath::round(static_cast<double>(a) / nearest) * nearest));
 	}
 
 	/**
 	 * @brief Rounds the input number downwards, effectively removing the decimal
 	 * point if it exists.
-	 * @returns An integer that has been rounded down.
+	 * @tparam T The type of number (int, float, double)
+	 * @returns A number that has been rounded down.
 	 */
 	template<class T>
-	SMATH_CONSTEXPR int floor(const T &a) {
+	SMATH_CONSTEXPR T floor(const T &a) {
 		SMATH_STATIC_ASSERT(smath::is_integer_type<T>::value || smath::is_floating_type<T>::value, "'floor' only accepts integer or floating-point inputs");
 		if (smath::is_integer_type<T>::value) {
 			return a;
 		}
-		return static_cast<int>((a > 0) ? a : a - 1);
+		return static_cast<T>(static_cast<int>((a > 0) ? a : a - 1));
+	}
+
+	/**
+	 * @brief Rounds each component of a vector downwards.
+	 * @tparam L The number of components in the vector in range [1, 4]
+	 * @tparam T The type of the vector (int, float, double)
+	 * @returns A vector of length `L` and type `T` with each component rounded down.
+	 */
+	template<length_t L, class T>
+	SMATH_CONSTEXPR vec<L, T> floor(const vec<L, T> &v) {
+		SMATH_STATIC_ASSERT(smath::is_integer_type<T>::value || smath::is_floating_type<T>::value, "'floor' only accepts integer or floating-point inputs");
+		SMATH_STATIC_ASSERT(smath::is_valid_vector(L), "'floor' only works on vectors with 1 to 4 components");
+		return function::one<vec, L, T, T>::apply(::std::floor, v);
 	}
 
 	/**
 	 * @brief Rounds the input number upwards.
-	 * @returns An integer that has been rounded upwards, regardless of decimal
-	 * point, if a decimal point exists.
+	 * @tparam T The type of number (int, float, double)
+	 * @returns A number that has been rounded upwards.
 	 */
 	template<class T>
-	SMATH_CONSTEXPR int ceil(const T &a) {
+	SMATH_CONSTEXPR T ceil(const T &a) {
 		SMATH_STATIC_ASSERT(smath::is_integer_type<T>::value || smath::is_floating_type<T>::value, "'ceil' only accepts integer or floating-point inputs");
 		if (smath::is_integer_type<T>::value) {
 			return a;
 		}
-		return static_cast<int>((a > 0) ? a + 1 : a);
+		return static_cast<T>(static_cast<int>((a > 0) ? a + 1 : a));
+	}
+
+	/**
+	 * @brief Rounds each component of a vector upwards.
+	 * @tparam L The number of components in the vector in range [1, 4]
+	 * @tparam T The type of the vector (int, float, double)
+	 * @returns A vector of length `L` and type `T` with each component rounded up.
+	 */
+	template<length_t L, class T>
+	SMATH_CONSTEXPR vec<L, T> ceil(const vec<L, T> &v) {
+		SMATH_STATIC_ASSERT(smath::is_integer_type<T>::value || smath::is_floating_type<T>::value, "'ceil' only accepts integer or floating-point inputs");
+		SMATH_STATIC_ASSERT(smath::is_valid_vector(L), "'ceil' only works on vectors with 1 to 4 components");
+		return function::one<vec, L, T, T>::apply(::std::ceil, v);
 	}
 
 	/**
 	 * @brief Scales the input value `x` from range [a, b] to range [c, d].
-	 * Credit to https://stats.stackexchange.com/a/281165.
+	 * Credit: https://stats.stackexchange.com/a/281165.
 	 * @param x The input value to scale.
 	 * @param a The minimum of the original range.
 	 * @param b The maximum of the original range.
 	 * @param c The new minimum of the range to scale to.
 	 * @param d The new maximum of the range to scale to.
-	 * @returns A scaled value.
+	 * @returns A scaled value in the range [c, d].
 	 */
 	template<class T>
 	SMATH_CONSTEXPR T scale(const T &x, const T &a, const T &b, const T &c, const T &d) {
