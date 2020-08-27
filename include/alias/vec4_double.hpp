99c808753fdc29c4fc4ccf765381991bad2249e2
Kyle Chapman <kyleichapman@gmail.com>
2020-08-27 23:32:46 +0200

    Add aliases for vectors with 4 components

---
+++ include/alias/vec4_double.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef ALIAS_VEC4_DOUBLE_H
+#define ALIAS_VEC4_DOUBLE_H
+
+#include "../types/type_vec4.hpp"
+
+namespace smath {
+
+	// Double-precision floating-point vector with 4 components
+	using vec4d = vec<4, double>;
+
+} // namespace smath
+
+#endif // ALIAS_VEC4_DOUBLE_H
---
+++ include/alias/vec4_float.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef ALIAS_VEC4_FLOAT_H
+#define ALIAS_VEC4_FLOAT_H
+
+#include "../types/type_vec4.hpp"
+
+namespace smath {
+
+	// Single-precision floating-point vector with 4 components
+	using vec4 = vec<4, float>;
+
+} // namespace smath
+
+#endif // ALIAS_VEC4_FLOAT_H
