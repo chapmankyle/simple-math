e1d19dedfb2a5b29d751c0fbf6fb0b5b0bfcc23d
Kyle Chapman <kyleichapman@gmail.com>
2020-09-01 16:00:11 +0200

    Change type of vector from struct to class

--- smath/types/qualifier.hpp
+++ smath/types/qualifier.hpp
@@ -20,11 +20,11 @@ namespace smath {
 	// - 4 components (x, y, z, w)
 
 	/**
-	 * General vector of arbitrary length and type.
+	 * General vector class of arbitrary length and type.
 	 * @tparam L The length of the vector, in range [1, 4]
 	 * @tparam T The type of data to store in the vector (bool, int, float or double)
 	 */
-	template<length_t L, class T> struct vec;
+	template<length_t L, class T> class vec;
 
 } // namespace smath
 
