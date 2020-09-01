a71e0bfb43222e2a2bce3a7b3f860e1a67d0748e
Kyle Chapman <kyleichapman@gmail.com>
2020-09-01 16:20:14 +0200

    Add support for functions taking in a vec4

--- smath/detail/function.hpp
+++ smath/detail/function.hpp
@@ -10,9 +10,10 @@ namespace smath {
 	namespace function {
 
 		/**
-		 * @brief Template class to execute a function with a single (one) parameter.
+		 * @brief Template class to execute a function with a single parameter.
 		 * @tparam L The number of components of the vector.
-		 * @tparam T The type of the vector (bool, int, double, float)
+		 * @tparam T The type of the given vector (bool, int, double, float)
+		 * @tparam A The return type of the vector (bool, int, double, float)
 		 */
 		template<template<length_t L, class T> class vec, length_t L, class A, class T>
 		struct one{};
@@ -38,10 +39,18 @@ namespace smath {
 			}
 		};
 
+		template<template<length_t L, class T> class vec, class A, class T>
+		struct one<vec, 4, A, T> {
+			SMATH_CONSTEXPR static vec<4, A> apply(A (*func) (T x), const vec<4, T> &v) {
+				return vec<4, A>(func(v.x), func(v.y), func(v.z), func(v.w));
+			}
+		};
+
 		/**
 		 * @brief Template class to execute a function with two parameters.
 		 * @tparam L The number of components of the vector.
-		 * @tparam T The type of the vector (bool, int, double, float)
+		 * @tparam T The type of the given vector (bool, int, double, float)
+		 * @tparam A The return type of the vector (bool, int, double, float)
 		 */
 		template<template<length_t L, class T> class vec, length_t L, class T>
 		struct two{};
@@ -64,6 +73,13 @@ namespace smath {
 		struct two<vec, 3, T> {
 			SMATH_CONSTEXPR static vec<3, T> apply(T (*func) (T x, T y), const vec<3, T> &a, const vec<3, T> &b) {
 				return vec<3, T>(func(a.x, b.x), func(a.y, b.y), func(a.z, b.z));
+			}
+		};
+
+		template<template<length_t L, class T> class vec, class T>
+		struct two<vec, 4, T> {
+			SMATH_CONSTEXPR static vec<4, T> apply(T (*func) (T x, T y), const vec<4, T> &a, const vec<4, T> &b) {
+				return vec<4, T>(func(a.x, b.x), func(a.y, b.y), func(a.z, b.z), func(a.w, b.w));
 			}
 		};
 
