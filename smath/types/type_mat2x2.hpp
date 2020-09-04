52ecd49095a75ff52b588cd37607af167a8a1801
Kyle Chapman <20703236@sun.ac.za>
2020-09-04 21:50:28 +0200

    Add constructor and element accesses

--- smath/types/type_mat2x2.hpp
+++ smath/types/type_mat2x2.hpp
@@ -19,17 +19,43 @@ namespace smath {
 	public:
 		// -- Useful type definitions --
 
-		using row_t = vec<m_cols, T>;
+		using row_t    = vec<m_cols, T>;
 		using column_t = vec<m_rows, T>;
 
-		using matrix_t = mat<2, 2, T>;
-		using transpose_t = mat<2, 2, T>;
+		using matrix_t    = mat<m_rows, m_cols, T>;
+		using transpose_t = mat<m_cols, m_rows, T>;
+
 		using value_t = T;
 
 	private:
 
-		// store vectors
+		// values in matrix stored as array of vectors
 		row_t values[m_length];
+
+	public:
+
+		/**
+		 * @returns The number of columns that the matrix has.
+		 */
+		static SMATH_CONSTEXPR length_t length() {
+			return m_length;
+		}
+
+		// -- Constructors --
+
+		SMATH_CONSTEXPR mat();
+
+		template<>
+		SMATH_CONSTEXPR mat(const mat<2, 2, T> &m);
+
+		explicit SMATH_CONSTEXPR mat(T scalar);
+		SMATH_CONSTEXPR mat(const T &x1, const T &y1, const T &x2, const T &y2);
+		SMATH_CONSTEXPR mat(const row_t &v1, const row_t &v2);
+
+		// -- Element accesses --
+
+		SMATH_CONSTEXPR row_t& operator[](length_t i);
+		SMATH_CONSTEXPR const row_t& operator[](length_t i) const;
 
 	};
 }
